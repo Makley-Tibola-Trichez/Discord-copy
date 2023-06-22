1d09f2e0bcdfa2db0008515bdbfeb9f0a6aea0cc
Makley Tibola Trichez <tibolamakley1@gmail.com>
2023-06-22 14:53:36 -0300

    refactor: adds column username to db, as a future way to add friends

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -13,6 +13,7 @@ export class AuthController {
     const _parsedBody = z
       .object({
         name: z.string().min(2),
+        username: z.string().min(4),
         email: z.string().email(),
         password: z.string().min(8),
       })
