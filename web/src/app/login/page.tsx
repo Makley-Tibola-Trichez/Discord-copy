7ca547a3447ef3bf327e8edf8fd2329d69fd9d4d
Makley <tibolamakley1@gmail.com>
2024-06-26 10:44:22 -0300

    refactor: removes phosphor-icons and lucide-icons

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,24 +1,24 @@
-import { CaretLeft } from "@phosphor-icons/react/dist/ssr";
+import { Icon } from "@iconify/react";
 import { Button } from "../../@/components/ui/button";
 import { Input } from "../../@/components/ui/input";
 import { loginAction } from "./login-submit";
 
 export default function Login() {
 	return (
-		<main className="bg-login-background h-screen w-screen">
+		<main className="h-screen w-screen bg-login-background">
 			<div className="flex items-center justify-center">
 				<form action={loginAction}>
-					<div className="bg-black bg-opacity-20 min-h-screen w-screen flex flex-1 justify-center items-center">
+					<div className="flex min-h-screen w-screen flex-1 items-center justify-center bg-black bg-opacity-20">
 						<div className="rounded bg-zinc-710 p-8">
-							<div className="flex items-center text-slate-300 hover:underline font-medium mb-4">
-								<CaretLeft weight="bold" className="mr-1" />
+							<div className="mb-4 flex items-center font-medium text-slate-300 hover:underline">
+								<Icon icon="ph:caret-left-bold" className="mr-1" />
 								<span>Voltar</span>
 							</div>
 							<div className="flex flex-col">
-								<div className="text-2xl font-bold text-zinc-300 flex justify-center">
+								<div className="flex justify-center font-bold text-2xl text-zinc-300">
 									Boas vindas de volta!
 								</div>
-								<div className="text-zinc-400 leading-5  flex justify-center">
+								<div className="flex justify-center text-zinc-400 leading-5">
 									Estamos muito animados em te ver novamente!
 								</div>
 							</div>
--- web/src/components/dialog/dialog.tsx
+++ web/src/components/dialog/dialog.tsx
@@ -1,66 +1,68 @@
-'use client';
+"use client";
 
-import { CaretLeft } from '@phosphor-icons/react';
-import { useId } from 'react';
-import { TextField } from '../textField';
-import { FormProvider, useForm } from 'react-hook-form';
-import { Input } from '../../@/form/textField';
-import { Button } from '../../@/components/ui/button';
+import { Icon } from "@iconify/react";
+import { FormProvider, useForm } from "react-hook-form";
+import { Button } from "../../@/components/ui/button";
+import { Input } from "../../@/form/textField";
 
 type DialogProps = {
-  open: boolean;
-  onClose(): void;
+	open: boolean;
+	onClose(): void;
 };
 
 export function Dialog() {
-  const _form = useForm();
-  return (
-    <div className='flex items-center justify-center'>
+	const _form = useForm();
+	return (
+		<div className="flex items-center justify-center">
+			<FormProvider
+				{..._form}
+				// onSubmit={_form.handleSubmit((v) => {
+				// console.log(v);
+				// })}
+			>
+				<form
+					onSubmit={_form.handleSubmit((v) => {
+						console.log({ v });
+					})}
+				>
+					<div className="flex min-h-screen w-screen flex-1 items-center justify-center bg-black bg-opacity-20">
+						<div className="rounded bg-zinc-710 p-8">
+							<div className="mb-4 flex items-center font-medium text-slate-300 hover:underline">
+								<Icon icon="ph:caret-left-bold" className="mr-1" />
+								<span>Voltar</span>
+							</div>
+							<div className="flex flex-col">
+								<div className="flex justify-center font-bold text-2xl text-zinc-300">
+									Boas vindas de volta!
+								</div>
+								<div className="flex justify-center text-zinc-400 leading-5">
+									Estamos muito animados em te ver novamente!
+								</div>
+							</div>
+							<div className="flex flex-col gap-2">
+								<Input
+									autoComplete="off"
+									name="email"
+									label="E-mail"
+									required
+								/>
+								<Input
+									autoComplete="off"
+									className="dark"
+									type="password"
+									required
+									label="Senha"
+									name="password"
+								/>
 
-      <FormProvider
-        {..._form}
-      // onSubmit={_form.handleSubmit((v) => {
-      // console.log(v);
-      // })}
-      >
-        <form
-          onSubmit={_form.handleSubmit((v) => {
-            console.log({ v });
-            
-          })}
-        >
-          <div className="bg-black bg-opacity-20 min-h-screen w-screen flex flex-1 justify-center items-center">
-            <div className="rounded bg-zinc-710 p-8">
-              <div className="flex items-center text-slate-300 hover:underline font-medium mb-4">
-                <CaretLeft weight="bold" className="mr-1" />
-                <>Voltar</>
-              </div>
-              <div className="flex flex-col">
-                <div className="text-2xl font-bold text-zinc-300 flex justify-center">
-                  Boas vindas de volta!
-                </div>
-                <div className="text-zinc-400 leading-5  flex justify-center">
-                  Estamos muito animados em te ver novamente!
-                </div>
-              </div>
-              <div className='flex flex-col gap-2'>
-
-              <Input
-                autoComplete='off'
-                name="email"
-                label="E-mail"
-                required
-                />
-              <Input
-                autoComplete='off' className='dark' type='password' required label='Senha' name='password' />
-
-            <Button className='w-full dark:text-zinc-200'  type="submit">Entrar</Button>
-                </div>
-            </div>
-          </div>
-        </form>
-      </FormProvider>
-    </div>
-
-  );
+								<Button className="w-full dark:text-zinc-200" type="submit">
+									Entrar
+								</Button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</FormProvider>
+		</div>
+	);
 }
